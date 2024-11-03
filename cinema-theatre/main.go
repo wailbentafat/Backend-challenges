a193e7a05baa175b66f2270cf0d48101556d1ee8
bentafat wail <150479778+wailbentafat@users.noreply.github.com>
2024-11-03 17:33:00 +0100

    kmltha

--- cinema-theatre/main.go
+++ cinema-theatre/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/gorilla/websocket"
 )
@@ -14,7 +17,61 @@ var  upgrader = websocket.Upgrader {
 	ReadBufferSize :1024,
 	WriteBufferSize :1024,
 }
+type  seats struct {
+	row int
+	seat int
+	booked bool
+}
+
+var seat [][] *seats
+
+func bind (message string)(string,string,string){
+	parts:=strings.Split(message, "/")
+	mes := parts[0]
+	seat:=parts[1]
+	row:=parts[2]
+	fmt.Println(mes,seat,row)
+	return mes,seat,row
+}
+
+func reserveSeat(seatNumber int, rowNumber int) (bool, string) {
+	if seat[rowNumber][seatNumber].booked {
+		return false, "already booked"
+	}
+
+	seat[rowNumber][seatNumber].booked = true
+	return true, "booked"
+}
+
+func initseats(){
+	
+	seat =make([][]*seats,10)
+	for i:= range seat{
+		seat[i]=make([]*seats,10)
+		for j:=0 ;j< 10;j++{
+	    
+	    seat[i][j]=&seats{i,j,false}
+		}
+	
+	}
+}
+func getSeats()([]byte){
+	var jsonSeat []byte
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+		jsonSeat, err := json.Marshal(seat)
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
+		fmt.Println(string(jsonSeat))
+		}
+		
+	}
+	return jsonSeat
+}
 func connectionHandler(w http.ResponseWriter, r *http.Request) {
+	initseats()
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println(err)
@@ -35,9 +92,35 @@ func connectionHandler(w http.ResponseWriter, r *http.Request) {
 
 	}
 }
-func treatmessages(){
-	
-}
+func treatmessages(message string){
+	msg,seat,row:=bind(message)
+	sea,err:=strconv.Atoi(seat)
+	print(err)
+	rowi,err:=strconv.Atoi(row)
+	print(err)
+
+	if  msg==""{
+		return
+	}
+	if msg=="make reservation"{
+	 ok,err:=reserveSeat(sea,rowi)
+	 if !ok{
+	 	fmt.Println(err)
+	 	return
+	 }
+	 if err==""{
+	 	fmt.Println("booked successfully")
+	 }
+
+     
+
+	}else if msg=="get seats"{
+		getSeats()
+	}
+
+
+	}
+
 
 
 func main() {
